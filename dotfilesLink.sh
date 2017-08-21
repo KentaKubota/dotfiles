6967b6c8c4cfe03c227df7b62512399ea11ae0df
Kenta Kubota <zingl.0523@gmail.com>
2017-08-22 00:35:27 +0900

    added .vimperatorrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
